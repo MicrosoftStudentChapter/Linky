761d9cb9533ef494c3f2442ea4b956165d86b66b
Jeevant Verma <jverma_be23@thapar.edu>
2024-06-20 01:10:12 +0530

    Added dynamic routing

---
+++ frontend/src/Adminpage.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+import { 
+  Grid
+ } from "@mui/material";
+
+ const Adminpage = () =>
+  {
+    return(
+      <Grid>
+        <h1>Admin Page</h1>
+      </Grid>
+    );
+  };
+
+export default Adminpage;
