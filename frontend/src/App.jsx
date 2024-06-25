757703ea6f2fdf0ac0c671c117311643652ca8c0
Jeevant Verma <jverma_be23@thapar.edu>
2024-06-25 17:21:47 +0530

    removed container from app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,29 +4,15 @@ import { Routes, Route, Link } from 'react-router-dom';
 import MainContentSection from './Maincontent';
 import Adminpage from './Adminpage';
 import "./App.css";
+import Administrators from './Adminpagecomponents/Administrators';
 
 const App = () => {
   return (
     <div className='app'>
-    <Container
-      maxWidth="md" 
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '89vh',
-        width: {
-          xs: '100%',
-          md: '50vw',
-        }
-      }}
-    >
       <Routes>
           <Route path="/" element={<MainContentSection />} />
           <Route path="/Adminpage" element={<Adminpage />} />
-        </Routes>
-      </Container>
-      
+        </Routes>     
     </div>
   );
 };
